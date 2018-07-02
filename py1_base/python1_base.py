555b90126a1900856d4792d6df53cda612c8a000
nanzong <zonzzz@126.com>
2018-07-02 17:47:56 +0800

    Initial commit

---
+++ py1_base/python1_base.py
@@ -0,0 +1,23 @@
+counter = 100  # 整形变量
+miles = 1000.0  # 浮点型变量
+name = "runoob"  # 字符串
+print(counter)
+print(miles)
+print(name)
+# 多个变量赋值
+# Python允许你同时为多个变量赋值。例如：
+a = b = c = 1
+# 多个对象指定多个变量。例如：
+a, b, c = 1, 2, "runoob"
+
+# 标准数据类型
+# Python3 中有六个标准的数据类型：
+# Number（数字）
+# String（字符串）
+# List（列表）
+# Tuple（元组）
+# Set（集合）
+# Dictionary（字典）
+# Python3 的六个标准数据类型中：
+# 不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）；
+# 可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）。
